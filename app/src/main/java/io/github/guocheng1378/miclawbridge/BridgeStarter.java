c6f2576e88a6f146bcd72c8cca618f4d8127148e
laishouchao <pasalai@qq.com>
2026-08-15 12:49:05 +0800

    chore: add build number to version log

--- app/src/main/java/io/github/guocheng1378/miclawbridge/BridgeStarter.java
+++ app/src/main/java/io/github/guocheng1378/miclawbridge/BridgeStarter.java
@@ -21,7 +21,7 @@ public class BridgeStarter {
             Config.loadFrom(appCtx);
             HttpServer server = new HttpServer(appCtx);
             server.start();
-            Logger.d("Miclaw API Bridge started (v3.4 voiceassist-channel-request)");
+            Logger.d("Miclaw API Bridge started (v3.4 voiceassist-channel-request) build=24");
         } catch (Throwable t) {
             Logger.e("Bridge start failed", t);
             started.set(false);
